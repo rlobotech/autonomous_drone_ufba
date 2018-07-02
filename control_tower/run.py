8ec3c110944dd5f4d87ea046d39f791a8ec2460b
Rafael Lobo <rlobotech@gmail.com>
2018-07-01 23:28:52 -0300

    Correcao do comentario no rum.py

--- control_tower/run.py
+++ control_tower/run.py
@@ -1,5 +1,5 @@
 from CT.app import app
 
-
+# Only executes if it was called from standard input, a script, or from an interactive prompt
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
